66dd9bb2944b39663948d17e4cd18092e0b8ab59
Perry <perry.wesel@gmail.com>
2016-04-29 15:36:00 +0200

    Css for the left side of the actionpanel

--- panorama/scripts/custom_game/hud.js
+++ panorama/scripts/custom_game/hud.js
@@ -1,11 +1,13 @@
 function Component(name, path) {
     var hud = $("#ZooHUD");
     var panel = $.CreatePanel( "Panel", hud, name );
-    panel.LoadLayoutAsync( "file://{resources}/layout/custom_game/"+path, false, false );                    
+    panel.LoadLayoutAsync( "file://{resources}/layout/custom_game/"+path, false, false );
+    $.Msg("Adding component ",name);                    
 }
 
 (function(){
     // Force reload
     var hud = $("#ZooHUD");
     hud.RemoveAndDeleteChildren();
+    $.Msg("reloading");
 })();
